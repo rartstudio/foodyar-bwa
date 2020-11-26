6eeca696a77693d42266d063073483cb5fb4d20c
rartstudio <58383924+rartstudio@users.noreply.github.com>
2020-11-26 19:42:29 +0700

    responsive header done

--- components/HeaderParts.vue
+++ components/HeaderParts.vue
@@ -1,29 +1,32 @@
 <template>
-    <header data-aos="fade-down" class="flex justify-between items-center relative">
-      <img src="~assets/images/logo.png">
-      <button class="flex flex-col md:hidden">
-        <span class="h-1 w-8 bg-gray-900 mb-1 rounded-3xl"></span>
-        <span class="h-1 w-8 bg-gray-900 mb-1 rounded-3xl"></span>
-        <span class="h-1 w-8 bg-gray-900 mb-1 rounded-3xl"></span>
-      </button>
-      <ul class="hidden md:block flex flex-col md:flex-row items-center">
-        <li>
-          <nuxt-link to="/" class="px-4 text-gray-500">Home</nuxt-link>
+    <header :class="[ToggleMenu ? 'pt-4': '',' flex justify-between items-center relative']">
+      <div :class="[ToggleMenu ? 'fixed z-50 -mx-4 px-4 pt-18' : '', 'flex justify-between w-full md:w-3/12 items-center']">
+        <img src="~assets/images/logo.png" class="z-50">
+        <div class="flex md:hidden">
+                <button @click="setToggleMenu(ToggleMenu)"
+                    :class="['toggle z-50', ToggleMenu ? 'active' : '']"
+                    >
+                </button>
+            </div>
+      </div>
+      <ul :class="['fixed inset-0 bg-gray-100 md:bg-transparent md:relative md:opacity-100 md:visible pt-24 md:pt-0 transition-colors duration-300  overflow-hidden md:flex flex-col md:flex-row items-center', ToggleMenu ? 'opacity-100 visible z-40' : 'opacity-0 invisible']">
+        <li class="my-4 md:my-0">
+          <nuxt-link to="/" class="px-4 text-lg text-gray-500">Home</nuxt-link>
         </li>
-        <li>
-          <nuxt-link to="/home" class="px-4 text-gray-500">Beverages</nuxt-link>
+        <li class="my-4 md:my-0">
+          <nuxt-link to="/home" class="px-4 text-lg text-gray-500">Beverages</nuxt-link>
         </li>
-        <li>
-          <nuxt-link to="/home" class="px-4 text-gray-500">Chef</nuxt-link>
+        <li class="my-4 md:my-0">
+          <nuxt-link to="/home" class="px-4 text-lg text-gray-500">Chef</nuxt-link>
         </li>
-        <li>
-          <nuxt-link to="/home" class="px-4 text-gray-500">Ingredient</nuxt-link>
+        <li class="my-4 md:my-0">
+          <nuxt-link to="/home" class="px-4 text-lg text-gray-500">Ingredient</nuxt-link>
         </li>
-        <li>
-          <nuxt-link to="/home" class="px-4 text-gray-500">Stages</nuxt-link>
+        <li class="my-4 md:my-0">
+          <nuxt-link to="/home" class="px-4 text-lg text-gray-500">Stages</nuxt-link>
         </li>
-        <li class="px-4 text-center">
-          <nuxt-link to="/login" class="ml-8 block btn-custom btn-login">
+        <li class="my-8 md:my-0 px-4 md:text-center">
+          <nuxt-link to="/login" class="md:ml-8 md:block text-lg btn-custom btn-login">
             My Kitchen
           </nuxt-link>
         </li>
@@ -32,13 +35,57 @@
 </template>
 
 <script>
-    export default {
-        
+     export default {
+        data() {
+          return {
+            ToggleMenu: false
+          }
+        },
+        methods: {
+            setToggleMenu(toggle){
+                return this.ToggleMenu = !toggle
+            }
+        },
     }
 </script>
 
 <style>
 .nuxt-link-active {
   @apply font-semibold text-gray-900;
+}
+
+.toggle {
+    position: relative;
+    width: 20px;
+    height: 20px;
+    z-index: 9999;
+}
+.toggle:focus {
+    outline: none
+}
+.toggle:before,
+.toggle:after{
+    @apply right-0 absolute bg-gray-900 transition-all duration-200;
+    content: "";
+    height:3px;
+    transform-origin:center;
+}
+.toggle:before{
+    top:26%;
+    width: 20px;
+}
+.toggle:after{
+    top: 58%;
+    width: 12px;
+}
+.toggle.active:before{
+    transform: translateY(-50%) rotate(-45deg);
+}
+.toggle.active:after{
+    transform: translateY(-50%) rotate(45deg);
+}
+.toggle.active:before, .toggle.active:after {
+    width: 20px;
+    top: 50%;
 }
 </style>
